f9f58dfadd198d424c3629dd6b613f9e3e994cf8
xuanniaomose <xuanniaomose@hotmail.com>
2023-03-03 10:33:32 +0800

    更新

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import re
 
 receive_buffer = " \n@FileMark@\n test文件.txt这是测试文件\nthis is a test file\n2023.3.1"
-msg_type = re.search(r"@\w{4}Mark@", receive_buffer, re.MULTILINE)
-print(msg_type.group(0))
+msg_type = re.findall(r"@(\w*)Mark@", receive_buffer, re.MULTILINE)[0]
+print(msg_type)

63045f99a7801f675a89ac7b8ced7717028b4a11
xuanniaomose <xuanniaomose@hotmail.com>
2023-03-10 23:36:53 +0800

    更新

--- Main.py
+++ Main.py
@@ -1,12 +1,15 @@
 import sys
-from PyQt6.QtWidgets import QMainWindow, QApplication
-from Server_GUI import Ui_XuanNiaoTR
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from XuanNiaoQuickTransfer import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.__ui = Ui_XuanNiaoTR()
+        self.__ui = Ui_MainWindow()
+        self.setWindowFlags(Qt.FramelessWindowHint)
         self.__ui.setupUi(self)
         self.setWindowTitle('玄鸟快传')  # 设置窗口标题要在窗口ui创建之后
 
